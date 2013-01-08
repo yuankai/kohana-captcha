68e1c791f1479f897b006151b33123fccf26779d
kevin <kai.yuan@foxmail.com>
2013-01-08 22:57:46 +0800

    change controller name to Captcha

--- init.php
+++ init.php
@@ -5,6 +5,6 @@ defined('SYSPATH') or die('No direct script access.');
 // Catch-all route for Captcha classes to run
 Route::set('captcha', 'captcha(/<group>)')
 	->defaults(array(
-	    'controller' => 'captcha',
+	    'controller' => 'Captcha',
 	    'action' => 'index',
 	    'group' => NULL));
